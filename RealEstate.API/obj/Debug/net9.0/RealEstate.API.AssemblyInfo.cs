26d20b0ce1ccb269f0e60fb25b6ca5e9ad19fef8
MarkoKajtazi <marko.kajtazi@students.finki.ukim.mk>
2026-02-12 09:38:25 +0100

    Fixing problem with the localhost allowed

--- RealEstate.API/obj/Debug/net9.0/RealEstate.API.AssemblyInfo.cs
+++ RealEstate.API/obj/Debug/net9.0/RealEstate.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealEstate.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f92e7fcc85d98a2ef8414c08e8c9cd18a3a1758")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71863ebd41356d07883736b8ac2899d61efbee6c")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealEstate.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealEstate.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
